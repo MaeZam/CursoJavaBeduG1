f72c1c8cf37c6933a77f9d98e14c34dbbb8283c2
Marlen Zamudio <130201740+Maerliin23@users.noreply.github.com>
2025-04-08 14:07:25 -0600

    Delete RETOS directory

--- RETOS/reto01/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        Paciente paciente = new Paciente();
-        paciente.guardarDatos();
-        paciente.mostrarInformacion();
-    }
-}
